05e40fc4ddb1fab28b376b49bdaffdaa12a3e1de
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-10-20 10:36:27 +0530

    updated the project
    Signed-off-by: Kartik Chauhan <lkchauhan.725@gmail.com>

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Aux from '../../hoc/abc'
 
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrawer, backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
